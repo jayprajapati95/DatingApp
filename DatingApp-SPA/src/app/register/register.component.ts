0eaa50fb56984c81f4eb804dbcedb275f7cd3bf9
Jay Prajapati <prajapatijay1995@gmail.com>
2018-09-30 22:13:57 +0530

    Section 6 half

--- DatingApp-SPA/src/app/register/register.component.ts
+++ DatingApp-SPA/src/app/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
+import { AlertifyService } from '../_services/Alertify.service';
 
 @Component({
   selector: 'app-register',
@@ -10,20 +11,19 @@ export class RegisterComponent implements OnInit {
   @Input() valuesFromHome: any; // @Input() is used to pass data component to component, parent use this.For this use [] inside html
   @Output() cancelRegister =  new EventEmitter(); // @Output() conatin emitter evemt, for this use ()  to access value inside html
   model: any = {};
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService, private alertify: AlertifyService) { }
 
   ngOnInit() {
   }
 
   register() {
     this.authService.register(this.model).subscribe(() => {
-      console.log('Registration Successfull');
+      this.alertify.success('Registration Successfull');
     }, error => {
-      console.log(error);
+      this.alertify.error(error);
     });
   }
   cancel() {
     this.cancelRegister.emit(false);
-    console.log('cancelled');
   }
 }
